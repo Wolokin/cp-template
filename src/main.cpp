ba004fdab49f84d7f9754da3bac9693c5fe0e692
Wolokin <fnikol22@gmail.com>
2021-09-20 17:59:36 +0200

    Slightly improved main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+#define ll long long
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
     return 0;
 }
