8c0f5900bcc300289f25564c25fbbb8b162c43c2
Wolokin <fnikol22@gmail.com>
2021-11-12 20:46:41 +0100

    Updated template

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,33 @@
 #include<bits/stdc++.h>
-
-#define VAR(i,n) __typeof(n) i = (n)
-#define loop(i,j,s) for(int i=j;i<s;i++)
-#define loopback(i,j,s) for(int i=j;i>=s;i--)
-#define foreach(i,c) for(VAR(i,(c).begin());i!=(c).end();i++)
-#define pln( x ) cout << x << "\n"
-#define ps( x ) cout << x << " "
-#define entr cout << "\n"
-#define pcnt(i) __builtin_popcount(i)
-#define ll long long
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define SIZE(c) (c).size()
-#define ALL(c) (c).begin(), (c).end()
+#pragma GCC optimize("Ofast")
 using namespace std;
-typedef vector<int> VI;
-typedef pair<int, int> PII;
-typedef vector<vector<int> > VVI;
-const int INFTY=20000000;
-const int MAX=500100;
-const int MOD=10000000;
+#define all(c) (c).begin(), (c).end()
+#define loop(i, a, b) for(int (i) = a; (i) < (b); (i)++)
+#define loopback(i, a, b) for(int (i) = a; (i) >= (b); (i)--)
 
-void coutTab(int* tab,int n){
-    loop(i,0,n){
-        cout<<tab[i]<<" ";
-    }
-    cout<<"\n";
-}
+typedef unsigned int uint;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vi > vvi;
+typedef vector<pii> vpi;
+const int INF = 1<<30, MOD = 1000000007, MAX = 200005;
 
-template<class T> void coutVec(vector<T> tab){
-    for(T t : tab){
-        cout<<t<<" ";
-    }
-    cout<<"\n";
+int popcount(uint i) { return __builtin_popcount(i); }
+template<class T> inline bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T> inline bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+template<class T> inline void yesno(bool f, const T& y = "YES", const T& n = "NO") { cout << (f ? y : n) << "\n"; }
+
+void solve() {
+    int n;
+    cin >> n;
 }
-//------------------------------------------
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) solve();
     return 0;
 }
