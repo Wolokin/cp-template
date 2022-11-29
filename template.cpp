77f924c2fd83a489ede13719bf35c70dd8cdbcea
Wolokin <fnikol22@gmail.com>
2022-11-29 18:55:31 +0100

    Fixed include, applied format

--- template.cpp
+++ template.cpp
@@ -1,12 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #pragma GCC optimize("Ofast")
 using namespace std;
 #define all(c) (c).begin(), (c).end()
-#define loop(i, a, b) for(int i = a; (i) < (b); (i)++)
-#define loopback(i, a, b) for(int i = a; (i) >= (b); (i)--)
+#define loop(i, a, b) for (int i = a; (i) < (b); (i)++)
+#define loopback(i, a, b) for (int i = a; (i) >= (b); (i)--)
 
 #ifdef LOCAL
-#include <debug.h>
+#include "debug.h"
 #else
 #define dbg(...)
 #endif
@@ -15,15 +15,34 @@ typedef unsigned int uint;
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
-typedef vector<vi > vvi;
+typedef vector<vi> vvi;
 typedef vector<pii> vpi;
-const int INF = 1<<30, MOD = 1000000007, MAX = 200005;
+const int INF = 1 << 30, MOD = 1000000007, MAX = 200005;
 
 int popcount(uint i) { return __builtin_popcount(i); }
-template<class T> inline bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
-template<class T> inline bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
-#define yesnoret(f) { yesno(f); return; }
-template<class T = string> inline void yesno(bool f, const T& y = "YES", const T& n = "NO") { cout << (f ? y : n) << "\n";}
+template <class T> inline bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T> inline bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+#define yesnoret(f)                                                            \
+    {                                                                          \
+        yesno(f);                                                              \
+        return;                                                                \
+    }
+template <class T = string>
+inline void yesno(bool f, const T &y = "YES", const T &n = "NO") {
+    cout << (f ? y : n) << "\n";
+}
 
 void solve() {
     int n;
@@ -35,6 +54,7 @@ int main() {
     cin.tie(nullptr);
     int t = 1;
     // cin >> t;
-    while(t--) solve();
+    while (t--)
+        solve();
     return 0;
 }
